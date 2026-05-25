5a5279cbbbbdc9198c795c37bf71d172760be6ed
DattolaFrancesco <dattolafrancescoo@gmail.com>
2026-05-25 23:40:32 +0200

    adds getter to video thumbnail

--- src/main/java/fra/skatemap/entities/Video.java
+++ src/main/java/fra/skatemap/entities/Video.java
@@ -2,15 +2,19 @@ package fra.skatemap.entities;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @DiscriminatorValue("video")
+@Getter
+@Setter
 public class Video extends Media {
-    private String ThumbnailUrl;
+    private String thumbnailUrl;
     public Video() {}
 
     public Video(Spot spot, String url, String publicId, String thumbnailUrl) {
         super(spot, url, publicId,"video");
-        this.ThumbnailUrl = thumbnailUrl;
+        this.thumbnailUrl = thumbnailUrl;
     }
 }
